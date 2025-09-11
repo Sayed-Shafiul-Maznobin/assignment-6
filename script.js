8ce7a176eef07ab33716c950b3d6ebe89b8f0e06
Sayed Shafiul Maznobin <sayedshafiulmaznobin@gmail.com>
2025-09-11 23:27:54 +0600

    cards loaded

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((json) => displayCategories(json.categories));
 };
-const loadCards = (id = {
-    
-});
+
+const loadCards = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => displayCards(json.plants));
+};
+
+//////////////////////////////////////////////////////////////////////
 const displayCategories = (lessons) => {
   const parent = document.getElementById("categoriesParent");
   parent.innerHTML = "";
@@ -18,7 +24,7 @@ const displayCategories = (lessons) => {
     
     
 
-        <button  onclick="loadCards(${lesson.id})"
+        <button  onclick="loadCards(${lesson.id}) "
           class="btn btn-wide hover:text-white hover:bg-[#15803d] bg-transparent border-none flex justify-start"
         >
           ${lesson.category_name}
@@ -28,6 +34,41 @@ const displayCategories = (lessons) => {
     parent.appendChild(child);
   }
 };
+const displayCards = (cards) => {
+  const parent = document.getElementById("card-container");
+  parent.innerHTML = "";
+  cards.forEach((card) => {
+    const child = document.createElement("div");
+    child.classList.add = "card  bbb  bg-white p-2 m-3";
 
+    child.innerHTML = `     
+          
+          <img
+            class="w-57 h-36 py-2   my-3 mx-auto rounded-2xl"
+            src="${card.image}"
+            alt=""
+          />        
+          <h3 class="py-1">${card.name}</h3>
+          <p class="text-gray-600 bbb h-[158px] py-1">${card.description}
+          </p>
+          <div class="flex justify-between py-1">
+            <div>
+              <button
+                class="btn btn-accent shadow-none bg-[#dcfce7] border-none rounded-3xl"
+              >
+                 ${card.category}  
+              </button>
+            </div>
+            <div class="flex items-center">   ${card.price}     </div>
+          </div>
+          <button
+            class="my-2 btn btn-block text-white bg-[#15803d] rounded-3xl"
+          >
+            Add to Cart
+          </button>
+         `;
+    parent.append(child);
+  });
+};
 ////////////////////////////////////////////////////
 loadCategories();
